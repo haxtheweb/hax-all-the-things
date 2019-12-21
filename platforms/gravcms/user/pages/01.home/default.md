24b67c85d12604d133175f45d924049934500fd1
btopro <btopro@outlook.com>
2019-12-21 16:27:18 -0500

    moved items for wp to work locally

--- platforms/gravcms/user/pages/01.home/default.md
+++ platforms/gravcms/user/pages/01.home/default.md
@@ -3,16 +3,13 @@ title: Home
 body_classes: 'title-center title-h1h2'
 ---
 
-<grid-plate layout="1-1" responsive-size="xl" style="width: 100%;"><h2 slot="col-1">An article about some jerk</h2>
+      <p></p>
+<p></p>
+<p></p>
+<p></p>
+<h2>Header</h2>
+<h2>Header</h2>
+<h2>Header</h2>
+<h2>Header</h2>
 
-
-<p slot="col-1">The hand.. ah yes.. that. So... It's part iron man, part stop. Mostly stop. As in... stop what you're doing the way you've been doing it, web components are the future.</p>
-
-
-<self-check style="width: 50%;" title="The future is in space" question="Microgravity" image="https://images-assets.nasa.gov/image/0000485/0000485~thumb.jpg" schema-resource-id="#8dd6cb15-7711-4d5d-721b" accent-color="cyan" resource="#8dd6cb15-7711-4d5d-721b" prefix="oer:http://oerschema.org/ schema:http://schema.org/ dc:http://purl.org/dc/terms/ foaf:http://xmlns.com/foaf/0.1/ cc:http://creativecommons.org/ns# bib:http://bib.schema.org " slot="col-2" alt="">  <span slot="question">Microgravity? lol no, the future is web components dawg!</span>
-  <span>Freal</span>
-</self-check>
-
-<meme-maker image-url="https://btopro.com/files/headshot.jpg" top-text="ugh" bottom-text="this guy" slot="col-1" style="width: 50%;"></meme-maker>
-
-</grid-plate>
+      
---
+++ platforms/wp-config.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ #ddev-generated: Automatically generated WordPress settings file.
+ ddev manages this file and may delete or overwrite the file unless this comment is removed.
+ */
+
+/** Authentication Unique Keys and Salts. */
+define('AUTH_KEY',         'vfZpXDBRbQuGPbhvSbqEwWPdsWbENrRjHXiexUAbuxapuiEaTpIzzqQxcpTREScQ');
+define('SECURE_AUTH_KEY',  'qQOqvARkmkrvxkxYsWhcGzeanhbvZIUjtlntmyGzHJeOMIZsDLZWfzruFEGfgvQb');
+define('LOGGED_IN_KEY',    'yoMOHsPqnVHgytHCXGUTjgHNACbJvRTlKkNmanngxnPxVfhtkOwCXeQyJkQZOrJE');
+define('NONCE_KEY',        'yyhiVXDkqMbkagmbutBCryiAnMIbjmLCQQImzgZGbCTJjKjvJLLQNiLoEUSKHlRe');
+define('AUTH_SALT',        'medpwPIvAlwZovtaClHAKgfCogJAUIIUqmOKPxugIxOQVkiXdgxflVdLzDRIqmuD');
+define('SECURE_AUTH_SALT', 'PsCIdakqbSyrOXnIAgGzSUWthkdRdLhOursREDSOyNWBqXhUfmXrbXGwSuWPKuFG');
+define('LOGGED_IN_SALT',   'fxeXraNayBEziSyUvazdujYoaLBZopbWCWEBCtofNmfpQzoxsAComfiMbGdYmmrY');
+define('NONCE_SALT',       'MmeYODPvYDjfHwtYOLcabKmuknChZCxLFPAVIrhGPLOZSvnJaXAybHObYHrMooLP');
+
+/** Absolute path to the WordPress directory. */
+define('ABSPATH', dirname(__FILE__) . '/');
+
+// Include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/wp-config-ddev.php';
+if (is_readable($ddev_settings) && !defined('DB_USER')) {
+	require_once($ddev_settings);
+}
+
+/** Include wp-settings.php */
+if (file_exists(ABSPATH . '/wp-settings.php')) {
+	require_once ABSPATH . '/wp-settings.php';
+}
